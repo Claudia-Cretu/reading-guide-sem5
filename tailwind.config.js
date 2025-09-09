5f81a3a666bc941e575bcfce679c6b4f78263917
Cretu Claudia <cretuclaudia373@yahoo.com>
2025-09-09 11:00:05 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        primary: '#6d201c',
+        secondary: '#8a3c32',
+        accent: '#c2ac93',
+        background: '#ddd0c2',
+      },
+      fontFamily: {
+        anton: ['Anton', 'sans-serif'],
+        publicsans: ['Public Sans', 'sans-serif'],
+        publicsans_thin: ['Public Sans', 'sans-serif'], // Use font-thin
+        publicsans_regular: ['Public Sans', 'sans-serif'], // Use font-normal
+      },
+    },
+  },
+  plugins: [],
+}
