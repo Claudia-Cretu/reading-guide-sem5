b6e73a3a95ba11f3c649527af72319b30d2072f7
Cretu Claudia <cretuclaudia373@yahoo.com>
2025-09-10 15:14:50 +0200

    Add version page component with HTML and CSS styling

---
+++ src/app/pages/version-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-version-page',
+  templateUrl: './version-page.component.html',
+  styleUrls: ['./version-page.component.css']
+})
+export class VersionPageComponent {}
